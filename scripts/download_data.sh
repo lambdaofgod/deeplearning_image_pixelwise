cbb6c0893918159d1d5a15fdf871057bd8c1dc38
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-04-29 18:00:12 +0200

    coco images and masks datasets

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,3 +1,4 @@
+wget -nc https://raw.githubusercontent.com/nightrome/cocostuff/master/labels.txt -O data/labels.txt
 for var in train2017 val2017
 do
 	wget -nc http://images.cocodataset.org/zips/$var.zip -O data/$var.zip
