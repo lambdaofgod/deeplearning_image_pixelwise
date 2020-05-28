d9de958370428fd425f472c49d303822156f7f8f
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-05-28 10:35:09 +0200

    moved hyperparams to Config file

--- download_data.sh
+++
@@ -1,4 +0,0 @@
-for var in train2017 val2017 stuffthingmaps_trainval2017 stuff_trainval2017 annotations_trainval2017
-do
-	wget -nc http://images.cocodataset.org/zips/$var.zip -O data/$var.zip
-done
